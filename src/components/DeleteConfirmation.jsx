3eb7f342548abdd2fea4329ae812216485a0faed
Ssewannonda Keith Edwin <skeith696@gmail.com>
2025-02-12 13:35:34 +0300

    refactor: optimise modal

--- src/components/DeleteConfirmation.jsx
+++ src/components/DeleteConfirmation.jsx
@@ -1,18 +1,7 @@
-import { useEffect, useState } from 'react'
-const TIMER = 3000
+import { useEffect } from 'react'
+import ProgressBar from './ProgressBar';
+const TIMER = 5000
 export default function DeleteConfirmation({ onConfirm, onCancel }) {
-  const [remainingTime, setRemainingTime] = useState(TIMER)
-
-  useEffect(() => {
-    const interval = setInterval(() => {
-      setRemainingTime(prevTime => prevTime - 10)
-    }, 10);
-
-    return () => {
-      clearInterval(interval)
-    }
-  }, [])
-  
   useEffect(() => {
     const timer = setTimeout(() => {
       onConfirm()
@@ -35,7 +24,7 @@ export default function DeleteConfirmation({ onConfirm, onCancel }) {
           Yes
         </button>
       </div>
-      <progress value={remainingTime} max={TIMER} />
+      <ProgressBar timer={TIMER} />
     </div>
   );
 }
